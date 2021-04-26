66c7683069679c6ac0ac786224f6ac08b21ce238
paulsimon33 <paulsim33@gmail.com>
2021-04-26 07:32:31 +0000

    deleted data and dowloaded _raw instead

---
+++ download.R
@@ -0,0 +1,13 @@
+rm(list = ls())
+library(stjudem)
+library(tidyverse)
+data(stjude)
+x <- stjude %>% 
+  pluck("expr") %>% 
+  as_tibble(rownames = NA)
+y <- stjude %>% 
+  pluck("labels") %>% 
+  as_tibble()
+
+write_tsv(x = x, file = "_raw/raw_stjude_x.tsv.gz")
+write_tsv(x = y, file = "_raw/raw_stjude_y.tsv.gz")
