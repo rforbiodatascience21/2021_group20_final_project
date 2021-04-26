1cfcb1e0c626134d718403d1bf43c9cfa1400147
paulsimon33 <paulsim33@gmail.com>
2021-04-26 08:00:28 +0000

    updated download

--- download.R
+++ download.R
@@ -8,6 +8,9 @@ x <- stjude %>%
 y <- stjude %>% 
   pluck("labels") %>% 
   as_tibble()
+rows <- rownames(x) %>% 
+  as_tibble()
 
 write_tsv(x = x, file = "_raw/raw_stjude_x.tsv.gz")
 write_tsv(x = y, file = "_raw/raw_stjude_y.tsv.gz")
+write_tsv(x = rows, file = "_raw/raw_stjude_xrows.tsv.gz")
