15ba3b49015da5e86a5138ffb129a8e2c31fb8e1
paulsimon33 <paulsim33@gmail.com>
2021-04-30 15:11:16 +0200

    added NAs count

--- R/02_clean.R
+++ R/02_clean.R
@@ -17,7 +17,11 @@ y_clean <- read_tsv(file = "./data/01_stjude_y.tsv.gz")
 # Wrangle data ------------------------------------------------------------
 stjude_clean <- as_tibble(cbind(y_clean,x_clean), rownames = NA)
 
+number_nas <- stjude_clean %>% 
+  summarise(NAs = sum(is.na(.)), .groups = "rowwise") %>% 
+  sum()
 
-# Write data --------------------------------------------------------------
+paste("The clean dataset has", number_nas, "NAs")
+  # Write data --------------------------------------------------------------
 write_tsv(x = stjude_clean,
          file = "data/02_stjude_clean.tsv.gz")
