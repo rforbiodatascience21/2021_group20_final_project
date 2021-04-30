fe519a391195cbbc9e3fea2cc1cae6ac3ddd33bf
paulsimon33 <paulsim33@gmail.com>
2021-04-30 21:43:10 +0200

    start of t-test

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -7,16 +7,38 @@ library("tidyverse")
 
 
 # Define functions --------------------------------------------------------
-source(file = "R/99_project_functions.R")
+# source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+st_jude <- read_tsv(file = "data/03_stjude_clean_aug.tsv.gz")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug %>% ...
+st_jude <- st_jude %>%
+  filter(value %in% c("BCR", "E2A", "Hyperdip", "MLL", "T", "TEL"))
 
+# Log transforming before t-test
+st_jude_log <- st_jude %>%
+  mutate_if(is.numeric, log)
+
+st_jude_long_nested <- st_jude_log %>% 
+  pivot_longer(cols = -value,
+               names_to = "gene",
+               values_to = "expr_level") %>% 
+  group_by(gene) %>% 
+  nest() %>% 
+  ungroup()
+
+# Idea that does not work for now
+st_jude_long_nested[[2]][[350]] %>%
+  group_by(value) %>% 
+  group_map(~ t.test(x = .x %>% select(expr_level), 
+                     y = st_jude_long_nested[[2]][[350]] %>% 
+                       filter(value == "BCR") %>% 
+                       select(expr_level)) %>% 
+              tidy() %>% 
+              select(statistic))
 
 # Model data
 my_data_clean_aug %>% ...
