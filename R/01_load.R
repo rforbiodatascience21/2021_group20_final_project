9278ed5889e290b2b7bde7fa13d56a310e2f05cc
elcatar <elcat@dtu.dk>
2021-05-01 22:05:30 +0000

    Updated load and clean to read in gene names

--- R/01_load.R
+++ R/01_load.R
@@ -4,6 +4,7 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("readxl")
 
 
 # Define functions --------------------------------------------------------
@@ -13,15 +14,26 @@ library("tidyverse")
 # Load data ---------------------------------------------------------------
 x <- read_tsv(file = "_raw/raw_stjude_x.tsv.gz")
 xrows <- read_tsv(file = "_raw/raw_stjude_xrows.tsv.gz")
-y <- read_tsv(file = "_raw/raw_stjude_y.tsv.gz")
+
+genes <- read_excel(path = "_raw/raw_bcr.xls")
 
 # Wrangle data ------------------------------------------------------------
 x <- t(x) %>% 
   as_tibble(rownames = NA)
 colnames(x) = t(xrows)
+
 x <- x %>% 
   mutate(id = rownames(x)) %>% 
-  relocate(id)
+  relocate(id) 
+
+genes <- as_tibble(genes) %>%
+  select(matches("Probe|Description")) %>%
+  filter(!(is.na(`Probe set`) & is.na(Descriptions)))
+
+
+
+
+
 # Write data --------------------------------------------------------------
 write_tsv(x = x, file = "data/01_stjude_x.tsv.gz")
-write_tsv(x = y, file = "data/01_stjude_y.tsv.gz")
+write_tsv(x = genes, file = "data/01_stjude_genes.tsv.gz")
--- R/02_clean.R
+++ R/02_clean.R
@@ -12,16 +12,23 @@ library("tidyverse")
 
 # Load data ---------------------------------------------------------------
 x_clean <- read_tsv(file = "./data/01_stjude_x.tsv.gz")
-y_clean <- read_tsv(file = "./data/01_stjude_y.tsv.gz")
+genes_clean <- read_tsv(file = "./data/01_stjude_genes.tsv.gz")
 
 # Wrangle data ------------------------------------------------------------
-stjude_clean <- as_tibble(cbind(y_clean,x_clean), rownames = NA)
 
-number_nas <- stjude_clean %>% 
+x_clean <- x_clean %>% 
+  separate(col = "id", 
+           into = c("leukemia", NA, NA, NA), 
+           sep = "-") %>%
+  mutate(sampleID = row_number()) %>%
+  relocate(sampleID)
+
+number_nas <- x %>% 
   summarise(NAs = sum(is.na(.)), .groups = "rowwise") %>% 
   sum()
 
 paste("The clean dataset has", number_nas, "NAs")
+
   # Write data --------------------------------------------------------------
 write_tsv(x = stjude_clean,
          file = "data/02_stjude_clean.tsv.gz")
