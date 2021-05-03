211609cb04a52912ae89e050942664bec30a7906
elcatar <elcat@dtu.dk>
2021-05-03 06:37:43 +0000

    rename columns with gene_name

--- R/02_clean.R
+++ R/02_clean.R
@@ -48,6 +48,9 @@ genes_clean <- genes_clean %>%
   ungroup() %>%
   select(-count)
 
+x_clean <- rename(x, !!! set_names(genes_clean$`Probe set`, genes_clean$Gene_name))
+
+
 # Write data --------------------------------------------------------------
 write_tsv(x = x_clean,
          file = "data/02_stjude_clean.tsv.gz")
