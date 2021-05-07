63393b8e2744f34a94c575dafd9ba37d042f7671
Elisa Catafal Tardos <elcat@dtu.dk>
2021-05-07 20:14:55 +0200

    added leukemia lables to the heatmaps

--- R/05_plots_heatmap.R
+++ R/05_plots_heatmap.R
@@ -54,21 +54,33 @@ p1 <- st_jude_top_10 %>%
    filter(leukemia %in% c("E2A", "MLL", "TEL"), 
           rank %in% c("E2A_top10", "MLL_top10", "TEL_top10")) %>%
    ggplot(mapping = aes(x = sampleID,
-                        y = fct_reorder2(gene, desc(expr_level), rank),
+                        y = fct_reorder2(gene, 
+                                         desc(expr_level), 
+                                         rank),
                         fill = expr_level)) + 
    geom_tile() +
-   scale_fill_gradient2(low = "green", 
-                        mid = "black", 
-                        high = "red", 
-                        midpoint = 0,
-                        limits = c(-4, 6)) +
-   scale_y_discrete(position = "right")+
+   scale_fill_gradientn(colors = c("green1", "green3", "black", "red3", "red1"),
+                        limits = c(-7, 7),
+                        name = "Expression level")  +
+   facet_grid( ~ leukemia, 
+               space="free", 
+               scales="free_x") +
+   scale_y_discrete(position = "right") +
+   theme_grey(base_size = 10) +
    theme(axis.text.x = element_blank(),
-         axis.title.y  = element_blank(),
+         axis.title  = element_blank(),
          axis.ticks = element_blank(),
-         axis.line.x.bottom = element_line(),
+         panel.border = element_blank(),
+         panel.spacing = unit(0, "points"),
+         panel.grid = element_blank(),
+         strip.background = element_rect(color = "white"),
+         strip.text = element_text(size = 10,
+                                   face = "bold"),
          legend.position = "bottom",
-         legend.direction = "horizontal")
+         legend.direction = "horizontal") +
+   guides(fill = guide_colorbar(title.position = "bottom",
+                                title.hjust = 0.5))
+  
 
 
 
@@ -77,39 +89,66 @@ p2 <- st_jude_top_10 %>%
    filter(leukemia %in% c("BCR", "Hyperdip", "T"), 
           rank %in% c("BCR_top10", "Hyperdip_top10", "T_top10"))%>%
    ggplot(mapping = aes(x = sampleID,
-                        y = fct_reorder2(gene, desc(expr_level), rank),
+                        y = fct_reorder2(gene, 
+                                         desc(expr_level), 
+                                         rank),
                         fill = expr_level)) + 
    geom_tile() +
-   scale_fill_gradient2(low = "green", 
-                        mid = "black", 
-                        high = "red", 
-                        midpoint = 0,
-                        limits = c (-4, 6.9)) +
-   scale_y_discrete(position = "right")+
+   scale_fill_gradientn(colors = c("green1", "green3", "black", "red3", "red1"),
+                        limits = c(-7, 7),
+                        name = "Expression level")  +
+   facet_grid( ~ leukemia, 
+               space="free", 
+               scales="free_x") +
+   scale_y_discrete(position = "right") +
+   theme_grey(base_size = 10) +
    theme(axis.text.x = element_blank(),
-         axis.title.y  = element_blank(),
+         axis.title  = element_blank(),
          axis.ticks = element_blank(),
-         axis.line.x.bottom = element_line(),
-         legend.position = "none")
+         panel.border = element_blank(),
+         panel.spacing = unit(0, "points"),
+         panel.grid = element_blank(),
+         strip.background = element_rect(color = "white"),
+         strip.text = element_text(size = 10,
+                                   face = "bold"),
+         legend.position = "none") 
+
+heatmap_1 <- p1+p2  
+   
+  
 
 p3 <- st_jude_top_10 %>%   
    filter(str_detect(rank, "top10"))%>%
    ggplot(mapping = aes(x = sampleID,
-                        y = fct_reorder2(gene, desc(expr_level), rank),
+                        y = fct_reorder2(gene, 
+                                         (expr_level), 
+                                         rank),
                         fill = expr_level)) + 
    geom_tile() +
-   scale_fill_gradient2(low = "deepskyblue1", 
-                        mid = "black", 
-                        high = "orange", 
-                        midpoint = 0,
-                        limits = c (-4, 6.9)) +
-   scale_y_discrete(position = "right")+
+   scale_fill_gradientn(colors = c("deepskyblue1", "black", "orange", "yellow"), 
+                        limits = c(-3.7, 6.9),
+                        name = "Expression level") +
+   facet_grid( ~ leukemia, 
+               space="free", 
+               scales="free_x") +
+   scale_y_discrete(position = "right") +
+   theme_grey(base_size = 9) + 
+   guides(fill = guide_colorbar(title.position = "top",
+                                title.hjust = 0.5,
+                                title.theme = element_text(size = 10))) +
    theme(axis.text.x = element_blank(),
-         axis.title.y  = element_blank(),
+         axis.title  = element_blank(),
          axis.ticks = element_blank(),
-         axis.line.x.bottom = element_line(),
+         panel.border = element_blank(),
+         panel.spacing = unit(0, "points"),
+         panel.grid = element_blank(),
+         strip.background = element_rect(color = "white"),
+         strip.text = element_text(size = 10,
+                                   face = "bold"),
          legend.position = "bottom",
          legend.direction = "horizontal")
+   
+
 
 
 #### Plots all top 10 genes
@@ -118,41 +157,63 @@ p1b <- st_jude_top_10 %>%
    filter(leukemia %in% c("E2A", "MLL", "TEL"), 
          str_detect(rank, "E2A|MLL|TEL"))%>%
    ggplot(mapping = aes(x = sampleID,
-                        y = fct_reorder2(gene, desc(expr_level), rank),
+                        y = fct_reorder2(gene, 
+                                         desc(expr_level), 
+                                         rank),
                         fill = expr_level)) + 
    geom_tile() +
-   scale_fill_gradient2(low = "green", 
-                        mid = "black", 
-                        high = "red", 
-                        midpoint = 0,
-                        limits = c(-4, 6.9)) +
-   scale_y_discrete(position = "right")+
+   scale_fill_gradientn(colors = c("green1", "green3", "black", "red3", "red1"),
+                        limits = c(-7, 7),
+                        name = "Expression level")  +
+   facet_grid( ~ leukemia, 
+               space="free", 
+               scales="free_x") +
+   scale_y_discrete(position = "right") +
+   theme_grey(base_size = 10) +
    theme(axis.text.x = element_blank(),
-         axis.title.y  = element_blank(),
+         axis.title  = element_blank(),
          axis.ticks = element_blank(),
-         axis.line.x.bottom = element_line(),
+         panel.border = element_blank(),
+         panel.spacing = unit(0, "points"),
+         panel.grid = element_blank(),
+         strip.background = element_rect(color = "white"),
+         strip.text = element_text(size = 10,
+                                   face = "bold"),
          legend.position = "bottom",
-         legend.direction = "horizontal")
+         legend.direction = "horizontal") +
+   guides(fill = guide_colorbar(title.position = "bottom",
+                                title.hjust = 0.5))
 
 
 p2b <- st_jude_top_10 %>% 
    filter(leukemia %in% c("BCR", "Hyperdip", "T"), 
           str_detect(rank, "BCR|Hyperdip|_T|T_"))%>%
    ggplot(mapping = aes(x = sampleID,
-                        y = fct_reorder2(gene, desc(expr_level), rank),
+                        y = fct_reorder2(gene, 
+                                         desc(expr_level), 
+                                         rank),
                         fill = expr_level)) + 
    geom_tile() +
-   scale_fill_gradient2(low = "green", 
-                        mid = "black", 
-                        high = "red", 
-                        midpoint = 0,
-                        limits = c (-4, 6.9)) +
-   scale_y_discrete(position = "right")+
+   scale_fill_gradientn(colors = c("green1", "green3", "black", "red3", "red1"),
+                        limits = c(-7, 7),
+                        name = "Expression level")  +
+   facet_grid( ~ leukemia, 
+               space="free", 
+               scales="free_x") +
+   scale_y_discrete(position = "right") +
+   theme_grey(base_size = 10) +
    theme(axis.text.x = element_blank(),
-         axis.title.y  = element_blank(),
+         axis.title  = element_blank(),
          axis.ticks = element_blank(),
-         axis.line.x.bottom = element_line(),
-         legend.position = "none")
+         panel.border = element_blank(),
+         panel.spacing = unit(0, "points"),
+         panel.grid = element_blank(),
+         strip.background = element_rect(color = "white"),
+         strip.text = element_text(size = 10,
+                                   face = "bold"),
+         legend.position = "none") 
+
+heatmap_2<- p1b+p2b 
 
 
 p3b <- st_jude_top_10 %>% 
@@ -160,20 +221,29 @@ p3b <- st_jude_top_10 %>%
                         y = fct_reorder2(gene, desc(expr_level), rank),
                         fill = expr_level)) + 
    geom_tile() +
-   scale_fill_gradient2(low = "deepskyblue1", 
-                        mid = "black", 
-                        high = "orange", 
-                        midpoint = 0,
-                        limits = c (-4, 6.9)) +
+   scale_fill_gradientn(colors = c("green1", "green3", "black", "red3", "red1"),
+                        limits = c(-7, 7),
+                        name = "Expression level")  +
+   facet_grid( ~ leukemia, space="free", scales="free_x") +
    scale_y_discrete(position = "right")+
    theme(axis.text.x = element_blank(),
-         axis.title.y  = element_blank(),
+         axis.title  = element_blank(),
          axis.ticks = element_blank(),
-         axis.line.x.bottom = element_line(),
+         panel.border = element_blank(),
+         panel.spacing = unit(0, "points"),
+         panel.grid = element_blank(),
+         strip.background = element_rect(color = "white"),
          legend.position = "bottom",
-         legend.direction = "horizontal")
+         legend.direction = "horizontal") +
+   guides(fill = guide_colorbar(title.position = "bottom"))
 
 
 
 
 # Write data --------------------------------------------------------------
+
+ggsave(plot = heatmap_1,
+      filename = "results/05_heatmap_1_pos.png")
+
+ggsave(plot = heatmap_2,
+       filename = "results/05_heatmap_2_top10.png")
