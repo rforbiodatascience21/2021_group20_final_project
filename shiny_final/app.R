7f3d997b2d998cf3fefc1a2c720b5127f8a3af32
Anna Oliver <s202436@student.dtu.dk>
2021-05-08 12:39:23 +0200

    add densitogram

--- shiny_final/app.R
+++ shiny_final/app.R
@@ -17,11 +17,14 @@ gene_names <-  st_jude_dwsz %>% select(-sampleID,-leukemia) %>% colnames()
 ui <- fluidPage(theme = shinytheme("flatly"),
 
     # Application title
-    titlePanel("Gene expression boxplot"),
-
+    titlePanel("Gene expression"),
+    hr(),
+    
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+            selectInput('plot_type','Select the plot type', c('Boxplot','Densitogram')
+                ),
             selectInput('gene','Select the gene you want to be displayed', gene_names)
         ),
 
@@ -34,8 +37,9 @@ ui <- fluidPage(theme = shinytheme("flatly"),
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    
+ 
     output$genePlot <- renderPlot({
+        if (input$plot_type == 'Boxplot') {
         ggplot(data = st_jude_dwsz,
                mapping = aes_string(x = "leukemia", 
                              y = input$gene, 
@@ -47,7 +51,19 @@ server <- function(input, output) {
                      caption = "Data source: Yeoh et al. (2002)")+
             guides(fill=guide_legend(title="Leukemia type"))+
             theme(plot.title = element_text(hjust = 0.5, face = "bold"))
-        })
+        }
+        else if (input$plot_type == 'Densitogram'){
+            ggplot(data = st_jude_dwsz,
+                   mapping = aes_string(x = input$gene, 
+                                        colour = "leukemia")) +
+            geom_density(size=1.2) +
+            labs(title = sprintf("Densitogram of %s", input$gene),
+                 y = 'Density',
+                 caption = "Data source: Yeoh et al. (2002)")+
+            guides(colour=guide_legend(title="Leukemia type"))+
+            theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+        }
+            })
 }
 
     
