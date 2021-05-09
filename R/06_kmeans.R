393471ea42c1f99d2144890fe62dac8354b64020
mspallou <m.spallou@gmail.com>
2021-05-09 15:31:47 +0200

    Added plots to kmeans

--- R/06_kmeans.R
+++ R/06_kmeans.R
@@ -12,10 +12,11 @@ library("ggplot2")
 top_40_genes <- read_tsv(file = "./data/03_stjude_downsized.tsv.gz")
 all_genes <- read_tsv(file = "./data/02_stjude_clean.tsv.gz")
 
-
+set.seed(20)
 kclust_top40 <- top_40_genes %>%
   as_tibble() %>% 
   select(c(-sampleID, -leukemia)) %>% 
+  scale() %>% 
   kmeans(centers = 6)
 
 clusters_top40 <- kclust_top40 %>% 
@@ -25,7 +26,16 @@ clusters_top40 <- kclust_top40 %>%
 
 clusters_count_top40 <- clusters_top40 %>%
   group_by(.cluster, leukemia) %>% 
+  count()
+  
   summarize(n = n())
+
+clusters_top40_plot <- clusters_count_top40 %>% 
+  ggplot(aes(.cluster, n, fill = leukemia)) +
+  geom_bar(stat = "identity")
+
+clusters_top40_plot
+  
 
 clusters_count_top40_filter <- clusters_top40 %>%
   group_by(.cluster, leukemia) %>% 
