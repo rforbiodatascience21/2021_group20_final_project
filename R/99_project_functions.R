3867c48780aba914bd859cf09bb9bac9e00deb27
RutMasdelesvalls <s203003@student.dtu.dk>
2021-05-10 19:58:51 +0200

    Update 99_project_functions.R

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -16,13 +16,23 @@ apply_t_test = function(data){
   return(my_list)
 }
 
+## EXPERIMENTAL FILTERING OF DATA USING ENTROPY
+
 # ENTROPY PER LEUKEMIA GROUPS
-# Count leukemia groups occurrences
-t <- st_jude_log %>% select(leukemia) %>% group_by(leukemia) %>% summarise(num = n()) %>% pull()
-# normalize the table (now a probability)
-p <- t %>% '/'(sum(t))
-# calculate entropy
-entropy <- -p %>% '*'(log(p, 2))
+entropy <- function(input.vector){
+  # Count leukemia groups occurrences
+  t <- st_jude_log %>% 
+    select(input.vector) %>% 
+    group_by(input.vector) %>% 
+    summarise(num = n()) %>% 
+    pull()
+  # normalize the table (now a probability)
+  p <- t %>% 
+    '/'(sum(t))
+  # calculate entropy
+  return(-p %>% 
+           '*'(log(p, 2)))
+}
 
 # ENTROPY PER GROUP PER GENE
 info.gain <- function(partition.by, categories){
@@ -61,48 +71,4 @@ info.gain <- function(partition.by, categories){
   
   return(abs(info.gain))
 }
-
-info.gain('X65962', 'leukemia')
-
-
-
-
-
-######## THE EXAMPLE OF HOW IT WORKS PER 1 GENE
-
-# Breakdown categories by partition.by
-partition.by <- st_jude_log %>% pull(X60957)
-categories <- st_jude_log %>% pull(leukemia)
-partition.table <- st_jude_log %>% 
-  select(X60957, leukemia) %>% 
-  arrange(X60957)
-
-# Count occurrences per partition in each group
-patientsnum <- nrow(partition.table)/2
-partition.table_g1 <- partition.table %>% top_n(n = patientsnum, wt = X60957)
-row.sums_g1 <- partition.table_g1  %>% group_by(leukemia) %>% summarise(num = n()) %>% pull()
-partition.table_g2 <- partition.table %>% top_n(n = patientsnum, wt = -X60957)
-row.sums_g2 <- partition.table_g2 %>% group_by(leukemia) %>% summarise(num = n()) %>% pull()
-
-# Normalize rows in each group
-prob1 <- partition.table_g1 %>% table() %>% '/'(row.sums_g1)
-prob2 <- partition.table_g2 %>% table() %>% '/'(row.sums_g2)
-
-# Calculate entropy per row in each group
-ent1 <- -prob1 %>% '*'(log(prob1, 2))
-ent2 <- -prob2 %>% '*'(log(prob2, 2))
-# we will define 0 * log(0) as 0
-ent1 <- ent1 %>% replace_na(0)
-ent2 <- ent2 %>% replace_na(0)
-
-# Calculate entropy per row adding groups
-ent <- rbind(ent1,ent2)
-
-# Calculate information gain as 
-# original entropy of categories - weighted average of entropy partitioned data
-#info.gain <- entropy(categories) - sum(ent * (row.sums / sum(row.sums)))
-result <- entropy - ent
-
-abs(result)
-
 
