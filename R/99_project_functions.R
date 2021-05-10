3e7c478e023cbc6c6c7dd41005b5be59899937dd
RutMasdelesvalls <s203003@student.dtu.dk>
2021-05-10 20:02:31 +0200

    Update 99_project_functions.R

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -35,6 +35,7 @@ entropy <- function(input.vector){
 }
 
 # ENTROPY PER GROUP PER GENE
+# ENTROPY PER GROUP PER GENE
 info.gain <- function(partition.by, categories){
   # Breakdown categories by partition.by
   #partition.by <- st_jude_log %>% pull(partition.by)
@@ -45,30 +46,47 @@ info.gain <- function(partition.by, categories){
   
   # Count occurrences per partition in each group
   patientsnum <- nrow(partition.table)/2
-  partition.table_g1 <- partition.table %>% top_n(n = patientsnum, wt = partition.by)
-  row.sums_g1 <- partition.table_g1  %>% group_by(categories) %>% summarise(num = n()) %>% pull()
-  partition.table_g2 <- partition.table %>% top_n(n = patientsnum, wt = -partition.by)
-  row.sums_g2 <- partition.table_g2 %>% group_by(categories) %>% summarise(num = n()) %>% pull()
+  partition.table_g1 <- partition.table %>% 
+    top_n(n = patientsnum, wt = partition.by)
+  row.sums_g1 <- partition.table_g1  %>% 
+    group_by(categories) %>% 
+    summarise(num = n()) %>% 
+    pull()
+  partition.table_g2 <- partition.table %>% 
+    top_n(n = patientsnum, wt = -partition.by)
+  row.sums_g2 <- partition.table_g2 %>% 
+    group_by(categories) %>% 
+    summarise(num = n()) %>% 
+    pull()
   
   # Normalize rows in each group
-  prob1 <- partition.table_g1 %>% table() %>% '/'(row.sums_g1)
-  prob2 <- partition.table_g2 %>% table() %>% '/'(row.sums_g2)
+  prob1 <- partition.table_g1 %>% 
+    table() %>% 
+    '/'(row.sums_g1)
+  prob2 <- partition.table_g2 %>% 
+    table() %>% 
+    '/'(row.sums_g2)
   
   # Calculate entropy per row in each group
-  ent1 <- -prob1 %>% '*'(log(prob1, 2))
-  ent2 <- -prob2 %>% '*'(log(prob2, 2))
+  ent1 <- -prob1 %>% 
+    '*'(log(prob1, 2))
+  ent2 <- -prob2 %>% 
+    '*'(log(prob2, 2))
   # we will define 0 * log(0) as 0
-  ent1 %>% replace_na(0)
-  ent2 %>% replace_na(0)
+  ent1 %>% 
+    replace_na(0)
+  ent2 %>% 
+    replace_na(0)
   
   # Calculate entropy per row adding groups
-  ent <- ent1 + ent2
+  ent <- ent1 %>% 
+    '+'(ent2)
   
   # Calculate information gain as 
   # original entropy of categories - weighted average of entropy partitioned data
   #info.gain <- entropy(categories) - sum(ent * (row.sums / sum(row.sums)))
-  info.gain <- entropy - ent
+  info.gain <- entropy %>% 
+    '-'(ent)
   
   return(abs(info.gain))
 }
-
