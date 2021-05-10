c042003cc76e1c3fad5a2e5757181a4f9b0f3aca
Anna Oliver <s202436@student.dtu.dk>
2021-05-10 08:36:37 +0200

    change location of st_jude_top_10 code

--- R/05_plots_heatmap.R
+++ R/05_plots_heatmap.R
@@ -12,41 +12,7 @@ library("patchwork")
 
 
 # Load data ---------------------------------------------------------------
-st_jude <- read_tsv(file = "data/03_stjude_downsized.tsv.gz")
-
-top_40_genes <- read_tsv(file = "data/03_top_40_genes.tsv.gz")
-
-
-# Wrangle data ------------------------------------------------------------
-
-top_10_genes <- top_40_genes %>%
-   group_by(leukemia) %>% 
-   top_n(n = 10, wt = ttest) %>% 
-   ungroup() %>%
-   arrange(leukemia, desc(ttest)) %>%
-   mutate(rank = case_when(ttest > 0 ~ str_c(leukemia, "_top10"),
-                           ttest < 0 ~ str_c("neg_corr_", leukemia)),
-          rank = fct_inorder(rank))%>%
-   select(gene, rank)
-
-
-gene_names <- top_10_genes %>%
-   pull(gene)
-
-st_jude_scaled <- st_jude %>%
-   mutate(sampleID = fct_inorder(sampleID),
-          leukemia = fct_inorder(leukemia),
-          across(c(-sampleID, -leukemia),
-                 scale))
-
-st_jude_top_10 <- st_jude_scaled %>%
-   select(sampleID, leukemia, all_of(gene_names)) %>%
-   pivot_longer(cols = c(-sampleID,-leukemia),
-                names_to = "gene",
-                values_to = "expr_level") %>%
-   left_join(top_10_genes,
-             by = c("gene"))
-
+st_jude_top_10 <- read_tsv(file = "data/03_st_jude_top_10.tsv.gz")
 
 ## Plots positively-correlated genes
 
